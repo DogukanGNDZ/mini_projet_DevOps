e49780c5e31c4be79a6ad2ad2166d5f4459992bb
Dogukan <dogukan.gunduz@student.vinci.be>
2021-11-24 16:19:24 +0100

    first function minute

---
+++ Berlin_Clock_Kata.php
@@ -0,0 +1,21 @@
+<?php
+
+
+public function minute($min): String{
+    if($min%5==0) {
+      return $ligne5="OOOO";
+    }
+    else if($min%5==1) {
+        return $ligne5="YOOO";
+    }
+    else if($min%5==2) {
+        return $ligne5="YYOO";
+    }
+    else if($min%5==3) {
+        return $ligne5="YYYO";
+    }
+    else{
+        return $ligne5="YYYY";
+    }
+}
+
