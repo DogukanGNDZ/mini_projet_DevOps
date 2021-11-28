0637b553191372946fdeef825aa69b3eda6bf580
Maxime Ferriere <maxime.ferriere@student.vinci.be>
2021-11-28 12:24:03 +0100

    test function minute 1

--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -1,1 +1,15 @@
 <?php
+use PHPUnit\Framework\TestCase;
+require_once("Berlin_Clock_Kata.php");
+class BerlinClockKataTest  extends TestCase{
+
+    public function testMinute1ShouldReturn1Lamp():void{
+        //Arrange
+        $Berlin_Clock = new Berlin_Clock_Kata();
+        //Act
+        $actual =$Berlin_Clock->minute(1);
+        //Assert
+        $this->assertEquals("YOOO",$actual);
+    }
+
+}
--- Berlin_Clock_Kata.php
+++ Berlin_Clock_Kata.php
@@ -1,21 +1,21 @@
 <?php
+class Berlin_Clock_Kata
+{
 
 
-public function minute($min): String{
-    if($min%5==0) {
-      return $ligne5="OOOO";
-    }
-    else if($min%5==1) {
-        return $ligne5="YOOO";
-    }
-    else if($min%5==2) {
-        return $ligne5="YYOO";
-    }
-    else if($min%5==3) {
-        return $ligne5="YYYO";
-    }
-    else{
-        return $ligne5="YYYY";
+    public function minute($min): string
+    {
+        if ($min % 5 == 0) {
+            return $ligne5 = "OOOO";
+        } else if ($min % 5 == 1) {
+            return $ligne5 = "YOOO";
+        } else if ($min % 5 == 2) {
+            return $ligne5 = "YYOO";
+        } else if ($min % 5 == 3) {
+            return $ligne5 = "YYYO";
+        } else {
+            return $ligne5 = "YYYY";
+        }
     }
 }
 
