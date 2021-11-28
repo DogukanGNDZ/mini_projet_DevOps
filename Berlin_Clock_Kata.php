d798f5fccad7420df8b611e42d30cdf275288cbb
Dogukan <dogukan.gunduz@student.vinci.be>
2021-11-28 12:28:41 +0100

    bug?

--- Berlin_Clock_Kata.php
+++ Berlin_Clock_Kata.php
@@ -1,21 +1,19 @@
 <?php
 
+    public function minute($min): string
+    {
+        if ($min % 5 == 0) {
+            return $ligne5 = "OOOO";
+        } else if ($min % 5 == 1) {
+            return $ligne5 = "YOOO";
+        } else if ($min % 5 == 2) {
+            return $ligne5 = "YYOO";
+        } else if ($min % 5 == 3) {
+            return $ligne5 = "YYYO";
+        } else {
+            return $ligne5 = "YYYY";
+        }
+    }
 
-public function minute($min): String{
-    if($min%5==0) {
-      return $ligne5="OOOO";
-    }
-    else if($min%5==1) {
-        return $ligne5="YOOO";
-    }
-    else if($min%5==2) {
-        return $ligne5="YYOO";
-    }
-    else if($min%5==3) {
-        return $ligne5="YYYO";
-    }
-    else{
-        return $ligne5="YYYY";
-    }
-}
+
 
