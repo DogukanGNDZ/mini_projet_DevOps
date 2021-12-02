a7f4f0ba964d0d06c1559f5e2688639fe4d8e11a
Maxime Ferriere <maxime.ferriere@student.vinci.be>
2021-12-02 16:03:07 +0100

    push refactored methode in other class

--- Berlin_Clock_Kata.php
+++ Berlin_Clock_Kata.php
@@ -3,6 +3,13 @@ class Berlin_Clock_Kata
 {
 
 
+    private refactoredMethod $refactoredMethod;
+
+    public function __construct()
+    {
+        $this->refactoredMethod = new refactoredMethod();
+    }
+
     public function minute($min): string
     {
         return $this->extracted3($min);
@@ -42,31 +49,7 @@ class Berlin_Clock_Kata
      */
     public function extracted($min): string
     {
-        if (floor($min / 5) == 11) {
-            return $ligne4 = "YYRYYRYYRYY";
-        } else if (floor($min / 5) == 10) {
-            return $ligne4 = "YYRYYRYYRYO";
-        } else if (floor($min / 5) == 9) {
-            return $ligne4 = "YYRYYRYYROO";
-        } else if (floor($min / 5) == 8) {
-            return $ligne4 = "YYRYYRYYOOO";
-        } else if (floor($min / 5) == 7) {
-            return $ligne4 = "YYRYYRYOOOO";
-        } else if (floor($min / 5) == 6) {
-            return $ligne4 = "YYRYYROOOOO";
-        } else if (floor($min / 5) == 5) {
-            return $ligne4 = "YYRYYOOOOOO";
-        } else if (floor($min / 5) == 4) {
-            return $ligne4 = "YYRYOOOOOOO";
-        } else if (floor($min / 5) == 3) {
-            return $ligne4 = "YYROOOOOOOO";
-        } else if (floor($min / 5) == 2) {
-            return $ligne4 = "YY0OOOOOOOO";
-        } else if (floor($min / 5) == 1) {
-            return $ligne4 = "YOOOOOOOOOO";
-        } else {
-            return $ligne4 = "OOOOOOOOOOO";
-        }
+        return $this->refactoredMethod->extracted($min);
     }
 
     /**
@@ -75,17 +58,7 @@ class Berlin_Clock_Kata
      */
     public function extracted1($heure): string
     {
-        if (floor($heure / 5) == 0) {
-            return $ligne3 = "OOOO";
-        } else if (floor($heure / 5) == 1) {
-            return $ligne3 = "ROOO";
-        } else if (floor($heure / 5) == 2) {
-            return $ligne3 = "RROO";
-        } else if (floor($heure / 5) == 3) {
-            return $ligne3 = "RRRO";
-        } else {
-            return $ligne3 = "RRRR";
-        }
+        return $this->refactoredMethod->extracted1($heure);
     }
 
     /**
@@ -94,17 +67,7 @@ class Berlin_Clock_Kata
      */
     public function extracted2($heure): string
     {
-        if ($heure % 5 == 0) {
-            return $ligne3 = "OOOO";
-        } else if ($heure % 5 == 1) {
-            return $ligne3 = "ROOO";
-        } else if ($heure % 5 == 2) {
-            return $ligne3 = "RROO";
-        } else if ($heure % 5 == 3) {
-            return $ligne3 = "RRRO";
-        } else {
-            return $ligne3 = "RRRR";
-        }
+        return $this->refactoredMethod->extracted2($heure);
     }
 
     /**
@@ -113,17 +76,7 @@ class Berlin_Clock_Kata
      */
     public function extracted3($min): string
     {
-        if ($min % 5 == 0) {
-            return $ligne5 = "OOOO";
-        } else if ($min % 5 == 1) {
-            return $ligne5 = "YOOO";
-        } else if ($min % 5 == 2) {
-            return $ligne5 = "YYOO";
-        } else if ($min % 5 == 3) {
-            return $ligne5 = "YYYO";
-        } else {
-            return $ligne5 = "YYYY";
-        }
+        return $this->refactoredMethod->extracted3($min);
     }
 
 }
